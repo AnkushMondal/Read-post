43b526fa8430880c726542bc70d743a9079b32cf
AnkushMondal <ankushmondal261103@gmail.com>
2026-02-15 23:57:42 +0530

    built a config file to set the .env folder

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,11 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
+//process the environment variable for react app--
+  // console.log(process.env.REACT_APP_APPWRITE_URL)
+
+//process the environment variable for vite app--
+  console.log(import.meta.env.VITE_APPWRITE_URL)
 
 
   return (
