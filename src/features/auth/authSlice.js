fab79c700188c6e3d90c8fb6861334da726731f5
AnkushMondal <ankushmondal261103@gmail.com>
2026-02-19 02:10:33 +0530

    feat: implement auth slice with login and logout reducers

---
+++ src/features/auth/authSlice.js
@@ -0,0 +1,26 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  status: false,
+  userData: null,
+};
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState,
+
+  reducers: {
+    login: (state, action) => {
+      state.status = true;
+      state.userData = action.payload.userData;
+    },
+    logout: (state) => {
+      state.status = false;
+      state.userData = null;
+    },
+  },
+});
+//export the actions to be used in the components 
+export const { login, logout } = authSlice.actions;
+//export the reducer to be used in the store
+export default authSlice.reducer;
