afb7b358cd7a6af35e28265bbee997154f839509
AnkushMondal <ankushmondal261103@gmail.com>
2026-02-25 19:47:48 +0530

    refactor(components): correct barrel exports and fix import paths

---
+++ src/components/index.js
@@ -0,0 +1,41 @@
+// Layout Components
+import Header from "./layout/Header/Header";
+import Footer from "./layout/Footer/Footer";
+import LogoutButton from "./layout/Header/LogoutButton";
+import Container from "./layout/Container/Container";
+
+// UI Components
+import Button from "./ui/Button";
+import Input from "./ui/Input";
+import Logo from "./ui/Logo";
+import Select from "./ui/Select";
+
+// Post Components
+import PostCard from "./Post/PostCard";
+import RTE from "./Post/RTE";
+import PostForm from "./Post/PostForm";
+
+import Signup from "./Signup";
+import Login from "./Login";
+import AuthLayout from "./AuthLayout";
+
+export {
+  // Layout
+  Header,
+  Footer,
+  LogoutButton,
+  Container,
+  // UI
+  Button,
+  Input,
+  Logo,
+  Select,
+  // Post
+  PostCard,
+  PostForm,
+  RTE,
+  Signup,
+  Login,
+  AuthLayout,
+};
+
