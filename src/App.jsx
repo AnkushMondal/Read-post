a0cf3d87a6dd67bec9ffa853fb61b16efec7985e
AnkushMondal <ankushmondal261103@gmail.com>
2026-02-25 19:59:36 +0530

    fix(app): resolve layout and routing issues in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,37 @@
-import { useState } from 'react'
-import './App.css'
-
+import { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import authService from "./appwrite/auth";
+import { login, logout } from "./features/auth/authSlice";
+import "./App.css";
+import { Footer, Header } from "./components";
+import { Outlet } from "react-router-dom";
 function App() {
-//process the environment variable for react app--
-  // console.log(process.env.REACT_APP_APPWRITE_URL)
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
 
-//process the environment variable for vite app--
-  console.log(import.meta.env.VITE_APPWRITE_URL)
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => setLoading(false));
+  }, []);
 
-
-  return (
-    <>
-      
-     
-    </>
-  )
+  return !loading ? (
+    <div className="min-h-screen flex flex-wrap content-between bg-gray-400">
+      <div className="w-full block">
+        <Header />
+        <main>
+          <Outlet />
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : null;
 }
-
-export default App
+export default App;
